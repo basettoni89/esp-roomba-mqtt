18ac46265d874629cc7221ee7db4e29f83e09d5a
Davide Mortara <dmortara@alteanet.it>
2020-05-01 11:32:12 +0200

    Updated .gitignore. Fix AndroidJson dep. Fix divide by zero on battery level

--- src/config.h
+++ src/config.h
@@ -7,7 +7,7 @@
 #define ADC_VOLTAGE_DIVIDER 44.551316985
 //#define ENABLE_ADC_SLEEP
 
-#define MQTT_SERVER "10.0.0.2"
-#define MQTT_USER "homeassistant"
-#define MQTT_COMMAND_TOPIC "vacuum/command"
-#define MQTT_STATE_TOPIC "vacuum/state"
+#define MQTT_SERVER "192.168.1.10"
+#define MQTT_USER "mqtt"
+#define MQTT_COMMAND_TOPIC "/home/roomba/command"
+#define MQTT_STATE_TOPIC "/home/roomba/state"
